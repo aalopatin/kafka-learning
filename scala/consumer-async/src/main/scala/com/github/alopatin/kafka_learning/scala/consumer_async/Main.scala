fc3a963f1cea878ac3b07d5d9d2f55c52e21d0a8
Alexander Lopatin <lopatin-disk@yandex.ru>
2024-12-05 19:52:30 +0300

    Consumer with async commit

--- scala/consumer-async/src/main/scala/com/github/alopatin/kafka_learning/scala/consumer_async/Main.scala
+++ scala/consumer-async/src/main/scala/com/github/alopatin/kafka_learning/scala/consumer_async/Main.scala
@@ -1,21 +1,21 @@
-package com.github.alopatin.kafka_learning.scala.consumer_sync
+package com.github.alopatin.kafka_learning.scala.consumer_async
 
 import com.sun.org.slf4j.internal.LoggerFactory
 import com.typesafe.config.ConfigFactory
-import org.apache.kafka.clients.consumer.{CommitFailedException, ConsumerConfig, KafkaConsumer}
-import org.apache.kafka.common.serialization.{DoubleDeserializer, StringDeserializer}
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer, OffsetAndMetadata}
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.StringDeserializer
 
 import java.time.Duration
+import java.util
 import java.util.{Collections, Properties}
 
 /**
- * Exersice: Write Kafka consumer that commits current offset synchronously.
+ * Exersice: Write Kafka consumer that commits current offset asynchronously.
  */
 object Main {
 
   private val config = ConfigFactory.load()
-
-  private val logger = LoggerFactory.getLogger(this.getClass)
 
   def main(args: Array[String]): Unit = {
 
@@ -24,7 +24,7 @@ object Main {
     val topic = args(0)
 
     val kafkaProperties = new Properties()
-    kafkaProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-sync")
+    kafkaProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-async")
     kafkaProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getString("kafka.bootstrap.servers"))
     kafkaProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
     kafkaProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
@@ -43,11 +43,13 @@ object Main {
         println(s"Poll number: $n")
         records.forEach(println(_))
         n += 1
-        try {
-          consumer.commitSync()
-        } catch {
-          case e: CommitFailedException => println(s"Commit failed: $e")
-        }
+
+        consumer.commitAsync((_: util.Map[TopicPartition, OffsetAndMetadata], exception: Exception) => {
+          if (exception != null) {
+            println(s"Commit error: $exception")
+          }
+        })
+
       }
     }
   }
